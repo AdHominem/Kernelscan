e017edbf0c930e06b1a2a398257bde7610fb110f
AdHominem <vargsmane@gmx.de>
2018-09-28 15:13:37 +0200

    Initial commit

---
+++ constants.py
@@ -0,0 +1,19 @@
+descriptions_file = 'descriptions.txt'
+kernel_config = 'kernel_3_18_113.config'
+
+types = ['def_bool',
+        'string',
+        'bool',
+        'int',
+        'tristate',
+        'boolean',
+        'hex',
+        'def_tristate']
+
+bool_types = ['def_bool',
+        'bool',
+        'tristate',
+        'boolean',
+        'def_tristate']
+
+path = 'C:\\Users\\...\\Documents\\P6 - KonnKonfig\\kernel_3.18.113_options_desc\\kernel_3.18.113_options_desc\\kernel_3.18.113_options_desc'
